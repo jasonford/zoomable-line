8fad6c46f2b9bebd038cfdea075356374f2d4bdb
Jason Ford <jason@workramp.com>
2019-12-02 22:25:04 -0800

    Make dates nice from months to billions of years

--- src/ZoomableLine.js
+++ src/ZoomableLine.js
@@ -9,31 +9,77 @@ const elementCenter = el => {
   }
 }
 
-const getPlaceValueTicks = (min, max, placeValue, label = l => l) => {
-  const numVisible = Math.ceil((max - min) / placeValue)
+const MONTHS = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ")
+
+const getPlaceValueTicks = (min, max, wholeUnit, fractionalUnit = 1, label = l => l) => {
+  const numVisible = Math.ceil((max - min) / wholeUnit * fractionalUnit);
   const ticks = [];
-  const scale = Math.min(1, 3 / (max - min) * 10 * placeValue);
+  const scale = Math.min(1, 20/(max - min) * wholeUnit / fractionalUnit);
   if (scale < 0.2) return [];
   for (let i=0; i<numVisible; i++) {
-    const x = Math.round((Math.ceil(min/placeValue) + i)/(1/placeValue));
-    if (x%(10*placeValue) !== 0) {
-      ticks.push({ label: `${label(x)}`, scale, x, y:0 });
-    }
+    const x = (Math.ceil(min/(wholeUnit/fractionalUnit)) + i) * wholeUnit / fractionalUnit;
+    ticks.push({
+      label: label(x),
+      scale,
+      nextX: (Math.ceil(min/(wholeUnit/fractionalUnit)) + i+1) * wholeUnit / fractionalUnit,
+      x,
+      y:0
+    });
   }
   return ticks;
 }
 
 const getTicks = (min, max) => {
+  const takenTickXValues = {}
+  const yearLabels = year => {
+    let yearString = Math.abs(year);
+    if (year % 100000000 === 0 && Math.abs(year) >= 1000000000) {
+      yearString = Math.abs(year) / 1000000000 + 'B';
+    }
+    else if (year % 100000 === 0 && Math.abs(year) >= 1000000) {
+      yearString = Math.abs(year) / 1000000 + 'M';
+    }
+    else if (year % 100 === 0 && Math.abs(year) >= 10000) {
+      yearString = Math.abs(year) / 1000 + 'K';
+    }
+    return <pre>{yearString} {year < 0 && 'bc'}</pre>;
+  }
+  const days = [];
+  const hours = [];
+  const minutes = [];
+  const seconds = [];
+  const milliseconds = [];
+  const microseconds = [];
+  const nanoseconds = [];
   return [
-    { label: '0', x: 0, y: 0, scale: 1 },
-    ...getPlaceValueTicks(min, max, 1/12, x => 'month'),
-    ...getPlaceValueTicks(min, max, 1),
-    ...getPlaceValueTicks(min, max, 10),
-    ...getPlaceValueTicks(min, max, 100),
-    ...getPlaceValueTicks(min, max, 1000),
-    ...getPlaceValueTicks(min, max, 10000),
-    ...getPlaceValueTicks(min, max, 100000)
-  ];
+    { label: <pre>0</pre>, x: 0, y: 0, scale: 1, nextX: 1 },
+    ...getPlaceValueTicks(min, max, 100000000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 10000000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 1000000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 100000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 10000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 1000000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 100000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 10000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 1000, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 100, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 10, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 1, 1, yearLabels),
+    ...getPlaceValueTicks(min, max, 1, 12, x => {
+      if (x < 0) x = x%1+1
+      return <pre>{MONTHS[Math.round(x%1*12)]}</pre>
+    })
+  ].map(
+    tick => {
+      if (takenTickXValues[tick.x]) {
+        return false;
+      }
+      else {
+        takenTickXValues[tick.x] = true;
+        return tick
+      }
+    }
+  ).filter( tick => tick );
 }
 
 export default class ZoomableLine extends React.Component {
@@ -116,11 +162,17 @@ export default class ZoomableLine extends React.Component {
             d => {
               const x = (d.x - this.state.min) / (this.state.max - this.state.min) * this.state.width;
               const y = this.state.height / 2;
+              const deltaToNextX = Math.abs(d.nextX - d.x);
+              const clickMin = d.x - deltaToNextX/3;
+              const clickMax = d.nextX + deltaToNextX/3;
+
               return (
                 <div
-                  key={d.label}
+                  key={d.x}
                   onWheel={ e => this.handleMouseWheel(e, elementCenter(e.target)) }
+                  onClick={ e => this.setState({min: clickMin, max: clickMax})}
                   style={{
+                    opacity: d.scale,
                     position: 'absolute',
                     fontSize: 24,
                     top: 0,
